51a3362e6b0edcba0303712eb9b26c3c552bf727
LiubovPil <alexandraelle95@gmail.com>
2022-08-12 13:56:33 +0300

    Adding prepared classes (token and condition_parser) and main file

--- Database/Database/Condition_parser.h
+++ Database/Database/Condition_parser.h
@@ -1,5 +1,13 @@
 #pragma once
-class Condition_parser
-{
-};
+
+#include "node.h"
+
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+shared_ptr<Node> ParseCondition(istream& is);
+
+void TestParseCondition();
 
--- Database/Database/Token.h
+++ Database/Database/Token.h
@@ -1,5 +1,22 @@
 #pragma once
-class Token
-{
+
+#include <sstream>
+#include <vector>
+using namespace std;
+
+enum class TokenType {
+	DATE,
+	EVENT,
+	COLUMN,
+	LOGICAL_OP,
+	COMPARE_OP,
+	PAREN_LEFT,
+	PAREN_RIGHT,
 };
 
+struct Token {
+	const string value;
+	const TokenType type;
+};
+
+vector<Token> Tokenize(istream& cl);
